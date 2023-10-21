6abdb13ddb0e4cb6fd45910da3238858982e5a6d
MUNEEBA MEHMOOD <124117980+engrmuneebamehmood@users.noreply.github.com>
2023-10-21 17:00:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+    carReducer,
+    addCar,
+    removeCar,
+    changeSearchTerm
+} from "./slices/carSlice";
+
+import{
+    formReducer,
+    changeName,
+    changeCost
+} from  "./slices/formSlice";
+
+const store= configureStore(
+    {
+        reducer:
+        {
+            cars:carReducer,
+            form:formReducer
+        }
+    }
+)
+
+export{
+    store,
+    changeName,
+    changeCost,
+    addCar,
+    removeCar,
+    changeSearchTerm
+}
